a97db938b5fe3cac0bdf9065723bdb29090b1bdc
Aman Kumar <125535132+Aman13052002@users.noreply.github.com>
2026-04-23 11:25:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	const strong = document.querySelector('p strong');
+	strong.forEach(ele=>{
+		ele.style.color = "green";
+	});
 }
 
 
 function return_normal() {
     //Write your code here
-
+	const back = document.querySelector('p strong');
+	back.forEach(ele=>{
+		ele.style.color = "black";
+	});
     
 }
